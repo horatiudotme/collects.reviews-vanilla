aab58c70482b81974c411bbdf1e57b7dc3c45bdb
helloworldwriter <writer@helloworldwriter.com>
2020-08-02 13:57:55 +0300

    Switch to defaultUrl

--- _eleventy/config.js
+++ _eleventy/config.js
@@ -24,6 +24,7 @@ module.exports = function (eleventyConfig) {
     },
     passthroughFileCopy: true,
     htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
     markdownTemplateEngine: "njk"
   }
 }
